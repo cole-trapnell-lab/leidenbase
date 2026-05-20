be619a9beb5ee8b3f8620a3a76596206c0276d6c
brgew <bge@uw.edu>
2026-05-20 10:43:04 -0700

    Revert "CRAN-related restriction modifications."
    This reverts commit e67bb6731b25c6d812073422eec20601a7f5ab3b.

--- vendor/arpack/dvout.f
+++ vendor/arpack/dvout.f
@@ -21,7 +21,7 @@
 *     ...
 *     ... SPECIFICATIONS FOR LOCAL VARIABLES
 *     .. Scalar Arguments ..
-      CHARACTER(LEN=*)   IFMT
+      CHARACTER*( * )    IFMT
       INTEGER            IDIGIT, LOUT, N
 *     ..
 *     .. Array Arguments ..
